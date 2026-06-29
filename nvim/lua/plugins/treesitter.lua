a6a3784ea7288cafce87ccd782b312873b0f90ff
Duk3-C <orlandojmarotogarcia@proton.me>
2026-06-29 19:47:07 -0400

    transitioned my nvim dotfiles alone to a different repository

--- nvim/lua/plugins/treesitter.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    otps = {
-      ensure_installed = { "lua", "c", "html", "javascript", "python", "cpp", "rust", "zig" },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
-}
